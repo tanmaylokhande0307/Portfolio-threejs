0730d7796934218cb51645f3ff79206203d20f5c
debian <tl@zasmlabs.com>
2024-12-17 23:38:50 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { logo } from "../assets";
+import { styles } from "../styles";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+            Tanmay &nbsp;
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
