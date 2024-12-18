a7f9c29f026c2441ad52389fff29513cc2089788
debian <tl@zasmlabs.com>
2024-12-19 00:51:43 +0530

    finished computer canvas section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { styles } from "../styles";
 import { ComputerCanvas } from "./canvas";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -24,7 +25,24 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputerCanvas /> 
+      <ComputerCanvas />
+      <div className="absolute xs:bottom-2 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] border-4 border-secondary flex p-2 rounded-3xl justify-center items-start">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop  ",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
