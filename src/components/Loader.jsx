95bcdcac84e494870a0dad08149b54f8c9c2194e
debian <tl@zasmlabs.com>
2024-12-19 00:17:59 +0530

    added CanvasLoader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
-  return (
-    <mesh>Loader</mesh>
-  )
-}
+  const { progress } = useProgress();
 
-export default Loader
+  return <Html>
+    <span className="canvas-load"></span>
+    <p className="text-[14px] text-[#f1f1f1] font-[800] mt-[40]">{progress.toFixed(2)}%</p>
+  </Html>;
+};
+
+export default Loader;
