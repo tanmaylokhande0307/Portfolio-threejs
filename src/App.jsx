91e78acbb0bf27370939c9c9d763e1556e0f4f10
debian <tl@zasmlabs.com>
2024-12-21 01:26:55 +0530

    added earth canvas and contact form

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
