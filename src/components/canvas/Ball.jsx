3f8b393551d979a1d0779189e639bd363a0c4353
debian <tl@zasmlabs.com>
2024-12-21 00:11:44 +0530

    added technologies

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,11 +1,36 @@
-import { Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import CanvasLoader from "../Loader";
 
 const Ball = ({ imgUrl }) => {
   const [decal] = useTexture([imgUrl]);
-  return <Float>Ball</Float>;
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color={"#fff8eb"}
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+        />
+      </mesh>
+    </Float>
+  );
 };
 
 const BallCanvas = ({ icon }) => {
