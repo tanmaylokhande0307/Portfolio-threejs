6bff0e516205ef42a14c78d3c433d522eae330d7
debian <tl@zasmlabs.com>
2024-12-21 16:12:54 +0530

    changed primitive position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`${styles.paddingX} absolute top-[120px] max-w-7xl inset-0 mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute top-[120px] max-w-7xl inset-0 mx-auto flex flex-row items-start gap-5 mb-3`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className={`h-5 w-5 rounded-full bg-[#915eff]`} />
